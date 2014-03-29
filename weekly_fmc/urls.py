a99e4eaaf72d086dafa11ada76370e011c45ec19
alotofdavid <david.adams94@gmail.com>
2014-03-29 13:32:55 -0700

    initial dev branch commit, now that app is live will be developing here

--- weekly_fmc/urls.py
+++ weekly_fmc/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import DetailView, ListView
 from fmc.models import Scramble
-
+from fmc import views
+from django.shortcuts import get_object_or_404
 from django.contrib import admin
 admin.autodiscover()
 
@@ -11,9 +12,8 @@ urlpatterns = patterns('',
             queryset=Scramble.objects.order_by('-pub_date')[:5],
             context_object_name='latest_scrambles',
             template_name='fmc/index.html')),
-    url(r'^(?P<pk>\d+)/$',
-        DetailView.as_view(
-            model=Scramble,
-            template_name='fmc/detail.html')),
+    url(r'^(?P<scramble_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<scramble_id>\d+)/results/$', views.results, name='results'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^(?P<scramble_id>\d+)/submit/$', views.submit, name='submit'),
 )
