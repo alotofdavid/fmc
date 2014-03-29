1550560b8e0a99262657674042f819897bcd244a
alotofdavid <david.adams94@gmail.com>
2014-03-29 13:32:55 -0700

    initial dev branch commit, now that app is live will be developing here

--- fmc/models.py
+++ fmc/models.py
@@ -8,12 +8,14 @@ class Scramble(models.Model):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=7)
 	def __unicode__(self):
 		return self.scramble;
+	end_date = timezone.now() + datetime.timedelta(days=7)
 	scramble = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 
 class Submission(models.Model):
 	def __unicode__(self):
 		return self.solution
+	comments = models.CharField(max_length=400)
 	scramble = models.ForeignKey(Scramble)
-	solution = models.CharField(max_length=200)
-	name = models.CharField(max_length=200)
+	solution = models.CharField(max_length=400)
+	name = models.CharField(max_length=50)
--- fmc/views.py
+++ fmc/views.py
@@ -1,19 +1,41 @@
 # Create your views here.
 from django.template import Context, loader
-from fmc.models import Scramble
+from django.utils import timezone
+from fmc.models import Scramble, Submission
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.core.urlresolvers import reverse
+import datetime
 
 def index(request):
 	latest_scrambles = Scramble.objects.all().order_by('-pub_date')[:5]
 	return render_to_response('fmc/index.html', { 'latest_scrambles': latest_scrambles })
 
-def root_index(request):
-	latest_scrambles = Scramble.objects.all().order_by('-pub_date')[:5]
-	return render_to_response('fmc/index.html', { 'latest_scrambles': latest_scrambles })
-
 def detail(request, scramble_id):
-    s = get_object_or_404(Scramble, pk=scramble_id)
-    return render_to_response('fmc/detail.html', {'scramble': s},context_instance=RequestContext(request))
+	s = get_object_or_404(Scramble, pk=scramble_id)
+	template = loader.get_template('fmc/detail.html')
+	context = RequestContext(request, {
+		'scramble': s
+	})
+	return HttpResponse(template.render(context))
+
+def submit(request, scramble_id):
+	s = get_object_or_404(Scramble, pk=scramble_id)
+	sol = request.POST['solution']
+	name = request.POST['name']
+	comments = request.POST['comments']
+	if not sol or not name:
+		return render_to_response('fmc/detail.html', {
+			'scramble': s,
+			'error_message': "Please complete all the fields.",
+			}, context_instance=RequestContext(request))
+	else:
+		sub = Submission(solution=sol, name=name,scramble_id=scramble_id)
+		sub.save()
+		return HttpResponseRedirect(reverse('results', args=(s.id,)))
+
+def results(request, scramble_id):
+	s = get_object_or_404(Scramble, pk=scramble_id)
+	submissions = s.submission_set.all()
+	return render_to_response('fmc/results.html', {'scramble': s, 'submissions': submissions})
