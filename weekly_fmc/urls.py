ffc2de7d87240f13d740e419dd968d0e4fcdd9d9
alotofdavid <david.adams94@gmail.com>
2014-04-16 17:04:20 -0700

    added login functionality

--- weekly_fmc/urls.py
+++ weekly_fmc/urls.py
@@ -11,9 +11,16 @@ urlpatterns = patterns('',
         ListView.as_view(
             queryset=Scramble.objects.order_by('-pub_date')[:5],
             context_object_name='latest_scrambles',
-            template_name='fmc/index.html')),
+            template_name='fmc/index.html'), name='index'),
     url(r'^(?P<scramble_id>\d+)/$', views.detail, name='detail'),
     url(r'^(?P<scramble_id>\d+)/results/$', views.results, name='results'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^(?P<scramble_id>\d+)/submit/$', views.submit, name='submit'),
+    url(r'^register/$', 'weekly_fmc.views.register'),
+    url(r'^login/$', 'weekly_fmc.views.login'),
+    url(r'^auth/$', 'weekly_fmc.views.auth_view'),
+    url(r'^logout/$', 'weekly_fmc.views.logout'),
+    url(r'^loggedin/$', 'weekly_fmc.views.loggedin'),
+    url(r'^invalid/$', 'weekly_fmc.views.invalid'),
+
 )
