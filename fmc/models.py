870b98575b71ae5b0db022a47737ce4d89d9478e
alotofdavid <david.adams94@gmail.com>
2014-06-09 18:50:08 -0700

    yay more stats

--- fmc/models.py
+++ fmc/models.py
@@ -13,8 +13,27 @@ class Scramble(models.Model):
 		return self.pub_date + datetime.timedelta(days=7)
 	def inverse(self):
 		return rubik.Algorithm(self.scramble).invert()
+
+	def rank(self):
+		arr = self.submission_set.order_by('move_count')
+		position_dict = {}
+		curr_position = 1
+		for i in range(len(arr)-1):
+			position_dict[arr[i]] = curr_position
+			if arr[i].move_count < arr[i+1].move_count:
+				curr_position += 1
+		#handle last index of array
+		position_dict[arr[len(arr)-1]] = curr_position
+		print(position_dict)
+		#TODO: store this dictionary in the database? I believe this runs in O(n) time so not a huge priority
+		for sub in arr:
+			position = position_dict[sub]
+			sub.ranking = position
+			sub.score = 4-position if position<4 else 0
+			sub.save()
 	def __unicode__(self):
 		return self.scramble;
+
 	scramble = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 	#end_date = timezone.now() + datetime.timedelta(days=7)
@@ -22,6 +41,9 @@ class Scramble(models.Model):
 class Submission(models.Model):
 	def __unicode__(self):
 		return self.solution
+
+	score = models.IntegerField()
+	ranking = models.IntegerField()
 	scramble = models.ForeignKey(Scramble)
 	user = models.ForeignKey(User, blank=True, null=True)
 	move_count = models.IntegerField()
