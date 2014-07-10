07723702475466f294cab3a30a13d2133ef24c4b
alotofdavid <david.adams94@gmail.com>
2014-07-10 13:25:29 -0700

    basic testing implemented

---
+++ fmc/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from fmc.models import Scramble, Submission
+from django.utils import timezone
+from rubik import *
+
+class ScrambleTestCase(TestCase):
+	def setUp(self):
+		Scramble.objects.create(scramble="R U R' U'", pub_date=timezone.now())
+
+	def test_animals_can_speak(self):
+		"""Tests algorithm creation and inversion. """
+		s = Scramble.objects.get(scramble="R U R' U'")
+		self.assertTrue(valid_alg(s.scramble))
+		a = Algorithm(s.scramble)
+		c = Cube()
+		c.apply_alg(a)
+		c.apply_alg(a.invert())
+		self.assertTrue(c.solved())
